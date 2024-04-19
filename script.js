81aded02f2174f5297d9fa7e0f50ab564eb6d89c
Prateek Jadhav <134202260+Prateek0712@users.noreply.github.com>
2024-04-19 12:20:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
-//your JS code here. If required.
- //DOM Elements
-const circles = document.querySelectorAll(".circle"),
-  progressBar = document.querySelector(".indicator"),
-  buttons = document.querySelectorAll("button");
+const circles = document.querySelectorAll('.circle');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
 
-let currentStep = 1;
+let currentIndex = 0;
 
-// function that updates the current step and updates the DOM
-const updateSteps = (e) => {
-  // update current step based on the button clicked
-  currentStep = e.target.id === "next" ? ++currentStep : --currentStep;
+// Initially set first circle as active
+circles[currentIndex].classList.add('active');
 
-  // loop through all circles and add/remove "active" class based on their index and current step
-  circles.forEach((circle, index) => {
-    circle.classList[`${index < currentStep ? "add" : "remove"}`]("active");
-  });
-
-  // update progress bar width based on current step
-  progressBar.style.width = `${((currentStep - 1) / (circles.length - 1)) * 100}%`;
-
-  // check if current step is last step or first step and disable corresponding buttons
-  if (currentStep === circles.length) {
-    buttons[1].disabled = true;
-  } else if (currentStep === 1) {
-    buttons[0].disabled = true;
-  } else {
-    buttons.forEach((button) => (button.disabled = false));
+// Event listener for next button
+nextButton.addEventListener('click', () => {
+  if (currentIndex < circles.length - 1) {
+    // circles[currentIndex].classList.remove('active');
+    circles[++currentIndex].classList.add('active');
+    prevButton.disabled = false;
   }
-};
+  if (currentIndex === circles.length - 1) {
+    nextButton.disabled = true;
+  }
+});
 
-// add click event listeners to all buttons
-buttons.forEach((button) => {
-  button.addEventListener("click", updateSteps);
+// Event listener for previous button
+prevButton.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    circles[currentIndex].classList.remove('active');
+    circles[--currentIndex].classList.add('active');
+    nextButton.disabled = false;
+  }
+  if (currentIndex === 0) {
+    prevButton.disabled = true;
+  }
 });
